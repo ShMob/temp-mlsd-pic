faf8ef00a4953a9b360321d02009d7ad1d388621
ShMob <mobashers313@gmail.com>
2023-08-10 09:40:18 +0330

    mlflow fix2

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-nginx -g "daemon off;" & uvicorn main:app --host 0.0.0.0 --port 8000 & mlflow server \
+nginx -g "daemon off;" & mlflow server \
     --backend-store-uri /mlflow_runs \
     --default-artifact-root /mlflow_artifacts \
     --host 0.0.0.0 \
-    --port 5000
+    --port 5000 \
+	& uvicorn main:app --host 0.0.0.0 --port 8000
